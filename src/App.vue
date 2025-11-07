fdbc31497fccf7dc8cb06bc754031e775d7fae99
noeyah2 <hy0708@cau.ac.kr>
2025-11-07 23:17:59 +0900

    feat(view): App.vue 에 router 관련 수정
    - DeployPage -> router-view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <DeployPage />
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import DeployPage from './views/DeployPage.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    DeployPage,
-  },
 });
 </script>
 
 <style>
-  body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 20px;
-    margin: 0;
-  }
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 0;
+}
 </style>
