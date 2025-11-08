1a8e62147565dcb51eb741972c93a4e699632909
Ji-su-Shin <caujisu2020@gmail.com>
2025-11-08 19:05:42 +0900

    feat(api): api-client 생성

---
+++ src/utils/api-client.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
+
+const API_BASE_URLS = import.meta.env.VITE_API_BASE_URL
+
+const createApiClient = (
+  baseURL: string,
+  contentType: string,
+): AxiosInstance => {
+  return axios.create({
+    baseURL,
+    headers: { 'Content-Type': contentType },
+    withCredentials: true,
+  });
+};
+
+export const apiClient = createApiClient(
+  API_BASE_URLS,
+  'application/json',
+);
